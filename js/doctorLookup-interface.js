a6d3e1225a1a9c0a12d48b322bd211a9aed89eb8
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-10-20 14:01:48 -0700

    Add ajax loading gif :)

--- js/doctorLookup-interface.js
+++ js/doctorLookup-interface.js
@@ -1,13 +1,20 @@
 import { DoctorLookup } from './../js/doctorLookup.js';
 
 $(document).ready(function(){
+  $(document).ajaxStart(function(){
+    $("#waiting").show();
+  });
+  $(document).ajaxComplete(function(){
+    $("#waiting").hide();
+    $("#results").show();
+ });
   $('form#doctor-search').submit(function(event){
     event.preventDefault();
     $('form#doctor-search').hide();
-    $('#results').show();
     let query = $('#query').val();
     let name = $('#name').val();
     let findDoctor = new DoctorLookup();
+
     if(query != '') {
       findDoctor.getDoctors(query);
     }else if(name != '') {
