ac9739b8a43e9eea6475439a21db871ccce919ee
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-10-20 09:46:45 -0700

    Add successful api call to backend.

--- js/doctorLookup-interface.js
+++ js/doctorLookup-interface.js
@@ -1,7 +1,11 @@
 import { DoctorLookup } from './../js/doctorLookup.js';
 
 $(document).ready(function(){
-  $('form#doctor-search').submit(function(){
-    
+  $('form#doctor-search').submit(function(event){
+    event.preventDefault();
+    $('form#doctor-search').hide();
+    let query = $('#query').val();
+    let findDoctor = new DoctorLookup();
+    findDoctor.getDoctors(query);
   });
 });
--- js/doctorLookup.js
+++ js/doctorLookup.js
@@ -2,14 +2,14 @@ var apiKey = require('./../.env').apiKey;
 
 export class DoctorLookup {
 
-  getDoctor(query) {
-    let address = userInput.replace(/\s/g, '+');
-    $.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ address }&key=AIzaSyAz2UbBTpj6yNEVN1eaPU6V2YIkenbGakg`)
+  getDoctors(query) {
+    let search = query.replace(/\s/g, '%20');
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${ search }&location=47.625703%2C-122.354744%2C50&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${ apiKey }`)
     .then(function(response){
-      
+      $('#results').append(response.data[0].profile.first_name);
     })
     .fail(function(){
-      console.log("something went wrong again")
+      console.log("something went wrong again");
     });
    }
 
